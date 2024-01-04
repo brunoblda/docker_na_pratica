ba3fd16450d4b8de70a092d54b28796b34abf0e7
brunoblda <brunoblda@gmail.com>
2024-01-04 17:21:29 -0300

    connection

--- run.py
+++ run.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from src import UserRepo
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def hello_world():
     return 'Estou na aplicação setada'
+
+@app.route('/insert', methods=['POST'])
+def insert():
+    
+    UserRepo = UserRepo()
+    body = request.json
+    
+    UserRepo.insert_user(body['name'])
+
+    return 'OK'
